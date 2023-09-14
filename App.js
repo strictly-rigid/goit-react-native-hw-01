f1f657d4c4475ba675c18542f855183be01d1e34
strictly-rigid <holysociology@gmail.com>
2023-09-14 21:42:26 +0300

    test commit hw2

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>
-        Open up App.js to start working on your app! This is Homework 1
+        Open up App.js to start working on your app! Starting Homework 2...
       </Text>
       <StatusBar style="auto" />
     </View>
